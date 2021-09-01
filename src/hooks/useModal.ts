2c173598cd0d8c6cbbc49d2df257fd6aa877c179
yujin mac <kimyu28jin@gmail.com>
2021-09-01 21:06:12 +0900

    Feat : useModal(hook) 추가

---
+++ src/hooks/useModal.ts
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+const useModal = (initialState = false) => {
+  const [opened, setOpened] = useState(initialState);
+
+  const openedModal = () => {
+    setOpened(true);
+  };
+
+  const closeModal = () => {
+    setOpened(false);
+  };
+  return [opened, openedModal, closeModal] as const;
+};
+
+export default useModal;
