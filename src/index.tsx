5218df8bf8e5fa8a231f232b0b2c46e5b5cfd468
yujin mac <kimyu28jin@gmail.com>
2021-09-01 10:07:59 +0900

    Chore & Style : styled-component install
    - styled-component, normalize
    - globalStyle 생성 및 적용

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,11 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./store";
+import GlobalStyle from "./styles/globalStyles";
 
 ReactDOM.render(
   <Provider store={store}>
+    <GlobalStyle />
     <App />
   </Provider>,
   document.getElementById("root")
